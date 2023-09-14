e2f338f75195e6fee8d006f43384eecf30d67185
alexpurs1980 <alexpurs@yandex.ru>
2023-09-14 16:31:59 +0300

    Initial Commit

---
+++ LandmarksSokolniki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LandmarksSokolniki
+//
+//  Created by Алексей Пурис on 14.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LandmarksSokolniki/LandmarksSokolnikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksSokolnikiApp.swift
+//  LandmarksSokolniki
+//
+//  Created by Алексей Пурис on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksSokolnikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
